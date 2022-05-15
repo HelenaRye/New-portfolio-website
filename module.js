b6e0488d6b4beec5173600bc657971ee07e982af
HelenaRye <78907672+HelenaRye@users.noreply.github.com>
2022-05-15 21:42:31 +0200

    Mobile navigation and visual changes

--- module.js
+++ module.js
@@ -1,9 +1,3 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-  smoothMobile: true,
-});
-
 const navSlide = () => {
   const burger = document.querySelector(".hamburger");
   const nav = document.querySelector(".nav-menu");
