1b8c33e7a1b82280a185b58a364c270580c6c0e3
HelenaRye <78907672+HelenaRye@users.noreply.github.com>
2022-05-16 11:41:17 +0200

    Moving js

--- script.js
+++ script.js
@@ -4,14 +4,4 @@ const scroll = new LocomotiveScroll({
   smoothMobile: true,
 });
 
-const navSlide = () => {
-  const burger = document.querySelector(".hamburger");
-  const nav = document.querySelector(".nav-menu");
 
-  burger.addEventListener("click", () => {
-    burger.classList.toggle("change");
-    nav.classList.toggle("nav-active");
-  });
-};
-
-navSlide();
