6161151bedb63173288bb3440506922e98cdb58a
HelenaRye <78907672+HelenaRye@users.noreply.github.com>
2022-05-21 18:54:29 +0200

    locomotive scroll implementation

---
+++ script.js
@@ -0,0 +1,30 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  multiplier: 1.0,
+  reloadOnContextChange: true,
+  touchMultiplier: 2,
+  smoothMobile: 0,
+  smartphone: {
+    smooth: false,
+  },
+  tablet: {
+    smooth: false,
+  },
+});
+
+setTimeout(() => {
+  scroll.update();
+}, 2000);
+
+// Scroll update all pages
+
+document.addEventListener("DOMContentLoaded", function () {
+  function ScrollUpdateDelay() {
+    setTimeout(function () {
+      scroll.update();
+    }, 500);
+  }
+
+  ScrollUpdateDelay();
+});
